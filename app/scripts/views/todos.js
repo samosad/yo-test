96afa270947e8be68dfb1cdde80b6f65c85f02f4
Aleksey Tabakman <samosad.ru@gmail.com>
2014-02-04 23:53:00 +0400

    todo add view

--- app/scripts/views/todos.js
+++ app/scripts/views/todos.js
@@ -7,7 +7,53 @@ yoTest.Views = yoTest.Views || {};
 
     yoTest.Views.TodosView = Backbone.View.extend({
 
-        template: JST['app/scripts/templates/todos.ejs']
+				el: '#todo-app',
+
+        template: JST['app/scripts/templates/todos.ejs'],
+
+        events: {
+          'submit': "createTodo"
+        },
+
+        initialize: function() {
+        	this.render();
+
+          this.listenTo(this.collection, 'add', this.addTodoItem);
+          this.listenTo(this.collection, 'reset', this.addAllTodoItems);
+
+          this.collection.fetch();
+        },
+
+        render: function() {
+          this.$el.html(this.template());
+
+          return this;
+        },
+
+        createTodo: function(e) {
+        	e.preventDefault();
+
+        	var title = this.$('#new-todo').val().trim();
+
+        	if (title) {
+        		this.collection.create(new yoTest.Models.TodoModel({
+        			title: title 
+        		}));
+
+        		this.$('#new-todo').val('');
+        	};
+        },
+
+        addTodoItem: function(todo) {
+        	var view = new yoTest.Views.TodosView({
+        		model: todo
+        	});
+        	this.$('ul').append(view.render().el);
+        },
+
+        addAllTodoItems: function() {
+        	this.collection.each(this.addTodoItem, this);
+        }
 
     });
 
